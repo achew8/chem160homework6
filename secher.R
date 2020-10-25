9ff47766d446caa2d7ebadb254b0af0dfa1e6ccb
achew8 <70230739+achew8@users.noreply.github.com>
2020-10-24 23:01:42 -0700

    Add files via upload

--- secher.R
+++ secher.R
@@ -1,3 +1,5 @@
+#bwt~bpd
+
 secher<-read.table("secher.txt",header=T)
 model<-lm(bwt~bpd,data=secher) 
 sum.model<-summary(model)
@@ -6,19 +8,17 @@ f<-sum.model$fstatistic
 p.value<-pf(f[1],f[2],f[3],lower.tail=F)
 output<-sprintf("R2 = %f and p-value=%f", R2, p.value)
 cat(output)
-
 intercept<-model$coefficients[1] 
 slope<-model$coefficients[2]
 output<-sprintf("slope=%f  intercept=%f",slope, intercept)
 cat(output)
-
 png("bwt_bpd.png")
 plot(bwt~bpd,data=secher) 
 abline(model)
 dev.off()
 
+#bwt~ad
 
-secher<-read.table("secher.txt",header=T)
 model<-lm(bwt~ad,data=secher) 
 sum.model<-summary(model)
 R2<-sum.model$r.squared
@@ -26,13 +26,29 @@ f<-sum.model$fstatistic
 p.value<-pf(f[1],f[2],f[3],lower.tail=F)
 output<-sprintf("R2 = %f and p-value=%f", R2, p.value)
 cat(output)
-
 intercept<-model$coefficients[1] 
 slope<-model$coefficients[2]
 output<-sprintf("slope=%f  intercept=%f",slope, intercept)
 cat(output)
-
-png("bwt_at.png")
+png("bwt_ad.png")
 plot(bwt~ad,data=secher) 
+abline(model)
+dev.off()
+
+#bwt~bpd+ad
+
+model<-lm(bwt~bpd+ad,data=secher) 
+sum.model<-summary(model)
+R2<-sum.model$r.squared
+f<-sum.model$fstatistic 
+p.value<-pf(f[1],f[2],f[3],lower.tail=F)
+output<-sprintf("R2 = %f and p-value=%f", R2, p.value)
+cat(output)
+intercept<-model$coefficients[1] 
+slope<-model$coefficients[2]
+output<-sprintf("slope=%f  intercept=%f",slope, intercept)
+cat(output)
+png("bwt~bpd+ad.png")
+plot(bwt~bpd+ad,data=secher) 
 abline(model)
 dev.off()
